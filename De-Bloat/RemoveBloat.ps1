9cf6d065ca85ae638f5a037a7d8e6a5bf8233741
Kjetil Furås <kjetil.furaas@gmail.com>
2023-05-22 12:25:18 +0200

    Update RemoveBloat.ps1

--- De-Bloat/RemoveBloat.ps1
+++ De-Bloat/RemoveBloat.ps1
@@ -992,6 +992,8 @@ $whitelistapps = @(
     "Google Chrome"
     "Microsoft Teams"
     "Teams Machine-Wide Installer"
+    "VMware Tools"
+    "Microsoft Visual C++*"
 )
 
 $InstalledSoftware = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
